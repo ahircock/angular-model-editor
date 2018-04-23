77bdddd276b26dd528442141d924c85ed115506f
ahircock <andrew.hircock@mxi.com>
2018-04-23 14:50:39 -0400

    Added all basic model attributes

--- src-angular/app/components/model-editor/model-editor.component.ts
+++ src-angular/app/components/model-editor/model-editor.component.ts
@@ -50,6 +50,9 @@ export class ModelEditorComponent implements OnInit {
 
   public MODEL_BASE_STATS = { Cost: 10, SPD: 5, EV: 5, ARM: 0, HP: 5, HIT: 6, DMG: 6 };
   public SPD_BONUS_COST = [ { Bonus: -2, Cost: -2 }, { Bonus: -1, Cost: -1 }, { Bonus: 0, Cost: 0 }, { Bonus: 1, Cost: 1 }, { Bonus: 2, Cost: 3 }, { Bonus: 3, Cost: 6 } ];
+  public EV_BONUS_COST = [ { Bonus: -2, Cost: -2 }, { Bonus: -1, Cost: -1 }, { Bonus: 0, Cost: 0 }, { Bonus: 1, Cost: 1 }, { Bonus: 2, Cost: 3 }, { Bonus: 3, Cost: 5 } ];
+  public ARM_BONUS_COST = [ { Bonus: 0, Cost: 0 }, { Bonus: 1, Cost: 1 }, { Bonus: 2, Cost: 2 }, { Bonus: 3, Cost: 4 }, { Bonus: 3, Cost: 6 } ];
+  public HP_BONUS_COST = [ { Bonus: 0, Cost: 0 }, { Bonus: 3, Cost: 3 }, { Bonus: 5, Cost: 5 } ];
 
   constructor( 
     private location: Location,
@@ -73,10 +76,10 @@ export class ModelEditorComponent implements OnInit {
     this.model.EVBonus = this.model.EV - this.MODEL_BASE_STATS.EV;
     this.model.ARMBonus = this.model.ARM - this.MODEL_BASE_STATS.ARM;
     this.model.HPBonus = this.model.HP - this.MODEL_BASE_STATS.HP;
-    this.model.SPDBonusCost = this.SPD_BONUS_COST.find( (element) => { return element.Bonus == this.model.SPDBonus; } ).Cost;;
-    this.model.EVBonusCost = 0;
-    this.model.ARMBonusCost = 0;
-    this.model.HPBonusCost = 0;
+    this.model.SPDBonusCost = this.SPD_BONUS_COST.find( (element) => { return element.Bonus == this.model.SPDBonus; } ).Cost;
+    this.model.EVBonusCost = this.EV_BONUS_COST.find( (element) => { return element.Bonus == this.model.EVBonus; } ).Cost;
+    this.model.ARMBonusCost = this.ARM_BONUS_COST.find( (element) => { return element.Bonus == this.model.ARMBonus; } ).Cost;
+    this.model.HPBonusCost = this.HP_BONUS_COST.find( (element) => { return element.Bonus == this.model.HPBonus; } ).Cost;
 
     // calculate the total cost
     this.updateCost();
@@ -86,16 +89,40 @@ export class ModelEditorComponent implements OnInit {
     this.location.back();
   }
 
-  selectSPDBonus( value: any ): void {
-    let SPDBonus = Number(value);
-    this.model.SPD = this.MODEL_BASE_STATS.SPD + SPDBonus;
-    this.model.SPDBonusCost = this.SPD_BONUS_COST.find( (element) => { return element.Bonus == SPDBonus; } ).Cost;
+  selectBonus( value: any, type: string ): void {
+    let Bonus = Number(value);
+
+    switch ( type ) {
+      case "SPD":
+        this.model.SPD = this.MODEL_BASE_STATS.SPD + Bonus;
+        this.model.SPDBonusCost = this.SPD_BONUS_COST.find( (element) => { return element.Bonus == Bonus; } ).Cost;
+        break;
+      case "EV":
+        this.model.EV = this.MODEL_BASE_STATS.EV + Bonus;
+        this.model.EVBonusCost = this.EV_BONUS_COST.find( (element) => { return element.Bonus == Bonus; } ).Cost;
+        break;
+      case "ARM":
+        this.model.ARM = this.MODEL_BASE_STATS.ARM + Bonus;
+        this.model.ARMBonusCost = this.ARM_BONUS_COST.find( (element) => { return element.Bonus == Bonus; } ).Cost;
+        break;
+      case "HP":
+        this.model.HP = this.MODEL_BASE_STATS.HP + Bonus;
+        this.model.HPBonusCost = this.HP_BONUS_COST.find( (element) => { return element.Bonus == Bonus; } ).Cost;
+        break;
+    }
+
+    // update the cost of the model
     this.updateCost();
   }
 
   updateCost(): void {
     this.model.cost = this.MODEL_BASE_STATS.Cost;
+
+    // add the model stat bonuses
     this.model.cost += this.model.SPDBonusCost;
+    this.model.cost += this.model.EVBonusCost;
+    this.model.cost += this.model.ARMBonusCost;
+    this.model.cost += this.model.HPBonusCost;
   }
 
 }

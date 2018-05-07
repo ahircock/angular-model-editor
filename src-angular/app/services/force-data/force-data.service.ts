c4e833152ef9312a69d19d166980a2dadeee94e2
ahircock <andrew.hircock@mxi.com>
2018-05-07 16:28:25 -0400

    Fixed up the Khorne entry in the fake DB

--- src-angular/app/services/force-data/force-data.service.ts
+++ src-angular/app/services/force-data/force-data.service.ts
@@ -23,7 +23,7 @@ export class ForceDataService {
 
   private forceDB: ForceData[] = [
     { _id:"F0001", name:"Templar Attack!", sizeName:"standard", maxCost: 200, cost:123, models:[{_id:"M0011",count:1},{_id:"M0003",count:2},{_id:"M0002",count:3}], equipment:[] },
-    { _id:"F0001", name:"Khorne Bloodbound", sizeName:"standard", maxCost: 200, cost:340, models:[{_id:"M0008",count:1},{_id:"M0007",count:3},{_id:"M0005",count:6}], equipment:[] },    
+    { _id:"F0002", name:"Khorne Bloodbound", sizeName:"standard", maxCost: 200, cost:340, models:[{_id:"M0008",count:1},{_id:"M0007",count:3},{_id:"M0005",count:6}], equipment:[] },    
   ];
 
   constructor() { }

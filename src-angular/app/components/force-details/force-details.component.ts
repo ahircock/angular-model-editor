862c2eb64d576e42e3dce0e18623e832327d435d
ahircock <ahircock@mxi.com>
2018-05-08 08:33:43 -0400

    Added the model editor to the force list

--- src-angular/app/components/force-details/force-details.component.ts
+++ src-angular/app/components/force-details/force-details.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { ForceData } from '../../services/force-data/force-data.service';
-import { ModelData } from '../../services/model-data/model-data.service';
+import { ForceDataService, ForceData } from '../../services/force-data/force-data.service';
+import { ModelData, ModelDataService } from '../../services/model-data/model-data.service';
 
 @Component({
   selector: 'app-force-details',
@@ -13,15 +13,73 @@ export class ForceDetailsComponent implements OnInit {
   public id: string;
   public force: ForceData;
   public models: ModelData[];
+  public selectedModel: ModelData;
+
+  // some cost counterse
+  public modelCost: number;
+  public equipmentCost: number;
 
   constructor(
-    private activatedRoute: ActivatedRoute
+    private activatedRoute: ActivatedRoute,
+    private modelDataService: ModelDataService,
+    private forceDataService: ForceDataService
   ) { }
 
-  ngOnInit() {
+  async ngOnInit() {
     this.id = this.activatedRoute.snapshot.paramMap.get("id");
-    // get the force-data
-    // get an array of models from the force-data
+
+    if ( this.id == "NEW") {
+      this.initNewForce();
+    } else {
+      await this.initForceFromId();
+    }
+
+    this.calculateCost();
+    
+    // mark the first model as selected
+    if ( this.models.length > 0 ) {
+      this.selectedModel = this.models[0];
+    }
+
+  }
+
+  private initNewForce() {
+    this.force = { _id:this.id, name:"New Force", sizeName:"Standard", maxCost:200, cost:0, models:[], equipment:[]};
+    this.models = [];
+  }
+
+  private async initForceFromId() {
+    this.force = await this.forceDataService.getForceById(this.id);    
+    let modelIdList: string[] = [];
+    for ( let forceModelData of this.force.models ) {
+      modelIdList.push( forceModelData._id );
+    }
+    this.models = await this.modelDataService.getModelListById( modelIdList );
+  }
+
+  onModelSelect( selectedModel: ModelData ) {
+    this.selectedModel = selectedModel;
+  }
+
+  newModelClick() {
+    let newModel = this.modelDataService.initNewModel();
+    this.force.models.push ( { _id:newModel._id, count:1 } );
+    this.models.push( newModel );
+    this.calculateCost();
+  }
+
+  calculateCost() {
+    
+    // get the total cost of models
+    this.modelCost = 0;
+    for ( let i=0; i<this.models.length; i++ ) {
+      this.modelCost += this.models[i].cost * this.force.models[i].count;
+    }
+
+    // get the total cost of equipment
+    this.equipmentCost = 0;
+
+    this.force.cost = this.modelCost + this.equipmentCost;
   }
 
 }

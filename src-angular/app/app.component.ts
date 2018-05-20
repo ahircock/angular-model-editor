8a8adcc35a58ad44da57bbda62ee9654a3a2b5ea
unknown <andrew.hircock@gmail.com>
2018-05-20 09:22:43 -0400

    Added some comments

--- src-angular/app/app.component.ts
+++ src-angular/app/app.component.ts
@@ -7,10 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
+  /**
+   * Used to show and hide the header band. By default the header is shown
+   */
   public showHeader:boolean = true;
 
+  /**
+   * this method should be called whenever a new route is handled, and the 
+   * router-outlet activates a new compoennt
+   * @param component the component that was activated in the router-outlet 
+   */
   routerOutletActivate( component: any ) {
 
+    // if the component in the router-outlet has a showHeader property, and 
+    // that property is set to false, then hide the header band
     if ( typeof component.showHeader == "boolean" && !component.showHeader ) {
       this.showHeader = false;
     } else {

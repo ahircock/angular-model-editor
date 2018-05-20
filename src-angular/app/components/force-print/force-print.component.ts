fafb4377fa25e88804c0583263e00487aa92db48
unknown <andrew.hircock@gmail.com>
2018-05-20 10:04:27 -0400

    Got the force printing working

--- src-angular/app/components/force-print/force-print.component.ts
+++ src-angular/app/components/force-print/force-print.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { ForceDataService, ForceData } from '../../services/force-data/force-data.service';
+import { ForceDataService, ForceData, ForceModelData } from '../../services/force-data/force-data.service';
 
 @Component({
   selector: 'app-force-print',
@@ -9,7 +9,19 @@ import { ForceDataService, ForceData } from '../../services/force-data/force-dat
 })
 export class ForcePrintComponent implements OnInit {
 
+  /**
+   * The details of the force being shown
+   */
   public force: ForceData;
+
+  public leftModels: ForceModelData[] = [];
+  public rightModels: ForceModelData[] = [];
+
+  /**
+   * This property is checked by the parent component, and 
+   * is used to show or hide the application header band
+   */
+  public showHeader: boolean = false;
 
   constructor(
     private activatedRoute: ActivatedRoute,
@@ -20,6 +32,18 @@ export class ForcePrintComponent implements OnInit {
     // load the force object
     let forceId = this.activatedRoute.snapshot.paramMap.get("id");
     this.force = await this.forceDataService.getForceById(forceId);
+
+    // add the first half of the models to the left array, and the second half to the right
+    for ( let i=0; i < this.force.models.length; i++ ) {
+      if ( i >= this.force.models.length/2 ) {
+        this.rightModels.push( this.force.models[i] );
+      } else {
+        this.leftModels.push( this.force.models[i] );
+      }
+    }
+
+    // open the print window
+    // window.print();
   }
 
 }

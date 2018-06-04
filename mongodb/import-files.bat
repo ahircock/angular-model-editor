3353dbffc1a4aa01aef4188214f1c97c5bcc3fdc
ahircock <ahircock@mxi.com>
2018-06-04 16:01:12 -0400

    Added a script to import files to local db

---
+++ mongodb/import-files.bat
@@ -0,0 +1,3 @@
+mongoimport --db model-editor --collection models --file mongo.models.json --jsonArray
+mongoimport --db model-editor --collection forces --file mongo.forces.json --jsonArray
+mongoimport --db model-editor --collection rules  --file mongo.rules.json  --jsonArray

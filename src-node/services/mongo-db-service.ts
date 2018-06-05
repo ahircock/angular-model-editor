46b4a0eacfca475fea807a38ee3ee0b2617bf742
ahircock <ahircock@mxi.com>
2018-06-05 10:27:21 -0400

    Separate the connect() method

--- src-node/services/mongo-db-service.ts
+++ src-node/services/mongo-db-service.ts
@@ -7,16 +7,26 @@ export class MongoDbService {
   private mongoConnection: MongoClient;
 
   /**
+   * Connect to the mongo database, and store the result in this.mongoConnection
+   */
+  async connect(): Promise<void> {
+
+    // if we aren't connected yet, then connect
+    if ( !this.mongoConnection ) {
+      this.mongoConnection = await MongoClient.connect(this.MONGO_URI);
+    }
+
+  }
+
+  /**
    * This method is used to retrieve the list of all documents from one collection
    * in the database. It will return an array of documents
    * @param collection the collection that will be searched for documents
    */
   async getAllDocuments(collection: string): Promise<any[]> {
 
-    // if we aren't connected yet, then connect
-    if ( !this.mongoConnection ) {
-      this.mongoConnection = await MongoClient.connect(this.MONGO_URI);
-    }
+    // make sure you have a connection to the database
+    await this.connect();
       
     // find the query and return the results as an array
     return await this.mongoConnection.db(this.MONGO_DB).collection(collection).find().toArray();

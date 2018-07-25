16aae17dada78a404961a6bf5e636e817defe02d
ahircock <andrew.hircock@mxi.com>
2018-07-25 12:54:26 -0400

    Abstract classes can't have @Component

--- src-angular/app/components/dropdowns/dropdown.component.ts
+++ src-angular/app/components/dropdowns/dropdown.component.ts
@@ -1,11 +1,5 @@
-import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
-import { ActionDataService } from '../../services/action-data/action-data.service';
+import { OnInit, Output, EventEmitter } from '@angular/core';
 
-@Component({
-  selector: 'app-dropdown-melee',
-  templateUrl: './dropdown-melee.component.html',
-  styleUrls: ['./dropdown-melee.component.css']
-})
 export abstract class DropdownComponent implements OnInit {
 
   /**

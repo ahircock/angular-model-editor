12d086abb918b143a6fa1553c167b18b3aec4bb3
unknown <andrew.hircock@gmail.com>
2018-07-24 21:16:21 -0400

    Remove old export/import files

--- mongodb/import-files-mlab.bat
+++
@@ -1,4 +0,0 @@
-mongoimport -h ds237669.mlab.com:37669 -u admin -p blix2400 --db model-editor --collection models --file mongo.models.json --jsonArray --drop
-mongoimport -h ds237669.mlab.com:37669 -u admin -p blix2400 --db model-editor --collection forces --file mongo.forces.json --jsonArray --drop
-mongoimport -h ds237669.mlab.com:37669 -u admin -p blix2400 --db model-editor --collection rules  --file mongo.rules.json  --jsonArray --drop
-mongoimport -h ds237669.mlab.com:37669 -u admin -p blix2400 --db model-editor --collection config --file mongo.config.json  --jsonArray --drop

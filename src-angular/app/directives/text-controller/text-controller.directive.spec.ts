99a06fd28d6194d5e7d67db18c6364054a1b91db
ahircock <andrew.hircock@mxi.com>
2018-08-14 12:17:44 -0400

    Removed the test stuff

--- src-angular/app/directives/text-controller/text-controller.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { TextControllerDirective } from './text-controller.directive';
-
-describe('TextControllerDirective', () => {
-  it('should create an instance', () => {
-    const directive = new TextControllerDirective();
-    expect(directive).toBeTruthy();
-  });
-});

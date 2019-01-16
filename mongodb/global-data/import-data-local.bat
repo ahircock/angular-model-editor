15a49bcb66c6e68ccf007a2a369445cb6cd07c94
Andrew Hircock <andrew.hircock@ifsworld.com>
2019-01-16 09:38:13 -0500

    Removed unnecessary spaces

--- mongodb/global-data/import-data-local.bat
+++ mongodb/global-data/import-data-local.bat
@@ -1,3 +1,3 @@
-mongoimport -d model-editor -c rules   --file rules.json   --jsonArray --drop 
+mongoimport -d model-editor -c rules   --file rules.json   --jsonArray --drop
 mongoimport -d model-editor -c actions --file actions.json --jsonArray --drop
 mongoimport -d model-editor -c models  --file models.json  --jsonArray --drop
